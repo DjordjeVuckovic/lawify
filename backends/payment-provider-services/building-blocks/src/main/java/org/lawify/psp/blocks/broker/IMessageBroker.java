e89d7f95e9064b2a69955f3a0ea575b8eb2ece66
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2023-11-17 23:19:25 +0100

    delete psp fronted because it will be implemented in react

--- backends/payment-provider-services/building-blocks/src/main/java/org/lawify/psp/blocks/broker/IMessageBroker.java
+++ backends/payment-provider-services/building-blocks/src/main/java/org/lawify/psp/blocks/broker/IMessageBroker.java
@@ -2,4 +2,5 @@ package org.lawify.psp.blocks.broker;
 
 public interface IMessageBroker {
     <T> void send(String destination, T message);
+    <T, R> R sendAndReceive(String destination, T request, Class<R> responseType);
 }
