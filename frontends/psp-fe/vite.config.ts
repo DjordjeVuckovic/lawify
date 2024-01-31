93dda9d020ecedb2e5652993e47f91c13782ebc7
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2024-01-31 02:52:27 +0100

    configure https for all services

--- frontends/psp-fe/vite.config.ts
+++ frontends/psp-fe/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react()
+    ],
+    server: {
+        https: {
+            https: true,
+            key: './certs/private-decrypted-key.pem',
+            cert: './certs/dev-cert.pem'
+        }
+    }
 })
